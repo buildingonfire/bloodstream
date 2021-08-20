3babc42b33c40a8fc73222f2c14e5c164521536c
Justin Dykhouse <buildingonfire@gmail.com>
2021-08-19 22:36:53 -0700

    updates!

---
+++ scenes/World.gd
@@ -0,0 +1,6 @@
+extends Node
+
+onready var Scoring = $Scoring
+
+func _ready():
+	pass # Replace with function body.
--- scenes/growth_body.gd
+++ scenes/growth_body.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal healed
+signal popped
+
 export(float) var growth_factor = 0
 export(float) var minimum_size = 0.25
 export(float) var maximum_size = 2.25
@@ -7,16 +10,12 @@ export(Color) var color_a = Color(1, 1, 1)
 export(Color) var color_b = Color(1, 0, 0)
 
 enum{
-	IDLE,
 	GROWING,
 	SHRINKING,
 	DEAD
 	}
 
 var prev_growth = growth_factor
-var growth_factor2 = Vector2(growth_factor, growth_factor)
-var minimum_size2 = Vector2(minimum_size, minimum_size)
-var maximum_size2 = Vector2(maximum_size, maximum_size)
 var state = GROWING
 var instability = 0.0
 
@@ -28,20 +27,17 @@ onready var label : Label = $Label
 
 func _process(_delta):
 	check_dead()
-	check_overlap()
-	print("scale= ", scale.x)
 	calculate_instability(scale.x)
-	print("instability = ", instability)
 	match state:
-		IDLE:
-			grow(-growth_factor/2)
 		GROWING:
 			grow(growth_factor)
+			growth_factor *= 1.001
 			sprite.modulate = lerp(color_a, color_b, instability)
 		SHRINKING:
 			grow(-growth_factor)
 		DEAD:
 			growth_factor = 0
+			print("dead")
 			queue_free()
 
 func grow(amt):
@@ -49,25 +45,21 @@ func grow(amt):
 	scale += amt2
 	prev_growth = amt
 
-func _on_Timer_timeout():
-	print("shrink timer expire")
-	state = GROWING
- 
-func _on_Area2D_body_exited(_body):
+func _on_Area2D_body_entered(_body):
 	state = SHRINKING
-	idleTimer.start()
-
-func check_overlap():
-	if area.get_overlapping_bodies().empty() == false:
-		state = SHRINKING
-		growth_factor += 0.001
 
 func check_dead():
-	if scale < minimum_size2 || scale > maximum_size2 * 2 :
+	if scale.x < minimum_size:
+		print("healed")
 		state = DEAD
+	elif scale.x > maximum_size :
+		print("popped")
+		state = DEAD
+	else:
+		pass
 
-func calculate_instability(val):
-		instability = remap_range(val, minimum_size, maximum_size, 0, 1)
+func calculate_instability(current_size):
+		instability = remap_range(current_size, minimum_size, maximum_size, 0, 1)
 
 func remap_range(value, InputA, InputB, OutputA, OutputB):
 	return(value - InputA) / (InputB - InputA) * (OutputB - OutputA) + OutputA
